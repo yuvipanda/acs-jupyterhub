7dbf0625a6069e8dcaf95b514836d0ddb6998987
yuvipanda <yuvipanda@gmail.com>
2017-07-19 12:37:35 -0700

    Initial commit

---
+++ start.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Start a cluster!
+set -e
+NAME=${1}
+
+sed "s/DNSPREFIX/${NAME}/" cluster.json > ${NAME}.json
+
+acs-engine generate ${NAME}.json
+
+az group create --name ${NAME} --location westus
+az group deployment create --name ${NAME} --resource-group ${NAME} --template-file ./_output/${NAME}/azuredeploy.json --parameters @./_output/${NAME}/azuredeploy.parameters.json
+sleep 1m
+scp -r -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no bootstrap/* datahub@${NAME}.westus.cloudapp.azure.com:
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no datahub@${NAME}.westus.cloudapp.azure.com "sudo bash setup-nfs.bash ${NAME}"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no datahub@${NAME}.westus.cloudapp.azure.com
