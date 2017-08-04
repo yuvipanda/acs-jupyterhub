738f90cee74a6e63a93b963fb69c215507f096a2
ryanlovett <rylo@berkeley.edu>
2017-08-04 15:35:38 -0700

    Separate ansible vars. Invoke ansible.

--- deploy.py
+++ deploy.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 
-# TODO: copy ssh key to master
-
 import argparse
 import json
 import os
@@ -27,23 +25,26 @@ parser.add_argument('-D', dest='disk_size', type=int, default=1024,
 	help='Disk size (gb) [default=1024].')
 args = parser.parse_args()
 
+# stash output data
 if not os.path.exists(args.name):
 	os.mkdir(args.name)
 elif not os.path.isdir(args.name):
 	print(args.name + " exists and is not a directory.")
 	sys.exit(1)
 
+# create an ssh keypair
 ssh_key = os.path.join(args.name, 'id_rsa-' + args.name)
 ssh_key_pub = ssh_key + '.pub'
-
 if not os.path.exists(ssh_key):
 	cmd = ['ssh-keygen', '-t', 'rsa', '-N', '', '-f', ssh_key]
 	r = sp.check_output(cmd)
 ssh_key_data = open(ssh_key_pub).read()
 
+# make sure we're using our subscription
 cmd = ['az', 'account', 'set', '-s', args.subscription_id]
 r = sp.check_output(cmd)
 
+# prepare az service principals
 rbac_file = os.path.join(args.name, args.rbac)
 if not os.path.exists(rbac_file):
 	cmd = ['az', 'ad', 'sp', 'create-for-rbac',
@@ -57,7 +58,8 @@ else:
 	rbac_s = open(rbac_file).read()
 rbac = json.loads(rbac_s)
 
-cluster = json.loads(open('cluster.json').read())
+# create acs-engine cluster file
+cluster = json.loads(open('templates/cluster.json.tmpl').read())
 cluster['properties']['masterProfile']['dnsPrefix'] = args.name
 cluster['properties']['servicePrincipalProfile']['servicePrincipalClientID'] = \
 	rbac["appId"]
@@ -65,14 +67,14 @@ cluster['properties']['servicePrincipalProfile']['servicePrincipalClientSecret']
 	rbac["password"]
 cluster['properties']['linuxProfile']['ssh']['publicKeys'][0]['keyData'] = \
 	ssh_key_data
-write_json(os.path.join(args.name, args.name + '.json'), cluster)
+write_json(os.path.join(args.name, 'cluster.json'), cluster)
 
-cmd = ['acs-engine', 'generate', os.path.join(args.name, args.name + '.json')]
+# run acs-engine
+cmd = ['acs-engine', 'generate', os.path.join(args.name, 'cluster.json')]
 r = sp.check_output(cmd)
 
 # create resource group
-cmd = ['az', 'group', 'create', '--name', args.name,
-	'--location', 'westus']
+cmd = ['az', 'group', 'create', '--name', args.name, '--location', 'westus']
 r = sp.check_output(cmd)
 
 # create the agents
@@ -90,9 +92,10 @@ cmd = ['az', 'network', 'vnet', 'list', '-g', args.name]
 vnet = json.loads(sp.check_output(cmd))
 agent_pool_vnet_name = vnet[0]['name']
 agent_pool_subnet_name = vnet[0]['subnets'][0]['name']
+agent_pool_subnet_address_prefix = vnet[0]['subnets'][0]['addressPrefix']
 
 # create nfs server
-vm_name = args.name + '-nfs'
+vm_name = 'nfs_server'
 cmd = ['az', 'vm', 'create', '-n', vm_name,
 	'--admin-username', 'datahub',
 	'--resource-group', args.name,
@@ -112,7 +115,7 @@ for i in range(1, args.disks + 1):
 		'--resource-group', args.name,
 		'--vm-name', vm_name,
 		'--size-gb', str(args.disk_size),
-		'--sku', 'Premium_LRS'])
+		'--sku', 'Premium_LRS']) 
 
 # run install script
 cmd = ['az', 'vm', 'extension', 'set',
@@ -123,12 +126,20 @@ cmd = ['az', 'vm', 'extension', 'set',
 	'--settings', './script-config.json']
 r = sp.check_output(cmd)
 
-# write out pv.yaml with our nfs server ip
+# write out pv.yaml and vars.yml with our network/nfs info
 nfs_host_ip = json.loads(vm_create)['privateIpAddress']
+
 pv = list(yaml.load_all(open('templates/pv.yaml.tmpl').read()))
 pv[1]['spec']['nfs']['server'] = nfs_host_ip
 f = open('bootstrap/pv.yaml', 'w')
 f.write(yaml.dump_all(pv, default_flow_style=False))
+f.close()
+
+ansible_vars = yaml.load(open('templates/vars.yml.tmpl').read())
+ansible_vars['nfs_client_subnet'] = agent_pool_subnet_address_prefix
+ansible_vars['nfs_server_ip'] = nfs_host_ip
+f = open('ansible/pv.yml', 'w')
+f.write(yaml.dump(ansible_vars, default_flow_style=False))
 f.close()
 
 # prepare to connect to master
@@ -140,14 +151,18 @@ os.environ['SSH_AUTH_SOCK'] = ''
 cmd = ['ssh'] + ssh_opts + [ssh_host, 'true']
 r = sp.check_output(cmd)
 
-# copy bootstrap code/data
-cmd = ['scp'] + ssh_opts + ['bootstrap/config.yaml', 'bootstrap/pv.yaml', 'bootstrap/setup.bash', ssh_host + ':']
+# copy ansible and bootstrap code/data
+cmd = ['scp'] + ssh_opts + ['bootstrap/config.yaml', 'bootstrap/pv.yaml', 'bootstrap/setup.bash', 'ansible/hosts', 'ansible/playbook.yml', ssh_host + ':']
 r = sp.check_output(cmd)
 
 # copy ssh keys
 cmd = ['scp'] + ssh_opts + [ssh_key,     ssh_host + ':.ssh/id_rsa']
 r = sp.check_output(cmd)
 cmd = ['scp'] + ssh_opts + [ssh_key_pub, ssh_host + ':.ssh/id_rsa.pub']
+r = sp.check_output(cmd)
+
+# run ansible
+cmd = ['ssh'] + ssh_opts + [ssh_host, 'ansible-playbook', '-i', 'hosts', 'playbook.yml']
 r = sp.check_output(cmd)
 
 # setup the cluster
