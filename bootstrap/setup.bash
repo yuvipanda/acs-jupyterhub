2fb85cb607c5586edce11b424ed7b02c19eda8b5
ryanlovett <rylo@berkeley.edu>
2017-08-04 17:45:45 -0700

    Set mode on export path. Set hosts to be executable.

--- bootstrap/setup.bash
+++ bootstrap/setup.bash
@@ -11,7 +11,7 @@ function proxy_public_ip {
 add-apt-repository -y ppa:ansible/ansible
 apt update > /dev/null
 apt -y install ansible
-ansible-playbook -i hosts playbook.yml
+sudo -u datahub -H ansible-playbook -i hosts playbook.yml
 
 curl -s -S https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
 helm init
@@ -23,9 +23,9 @@ echo 'done waiting'
 helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
 helm repo update
 
-helm install jupyterhub/jupyterhub --version=v0.4 --name=jupyterhub --namespace=jupyterhub -f config.yaml
-
-# kubectl apply -f pv.yaml
+helm install jupyterhub/jupyterhub --version=v0.4 \
+	--name=jupyterhub --namespace=jupyterhub -f \
+	config.yaml
 
 PUBLIC_IP=$(proxy_public_ip)
 while [ "${PUBLIC_IP}" == '<pending>' ]; do
