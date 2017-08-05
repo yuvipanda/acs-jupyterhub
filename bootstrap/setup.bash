b3d051a4dccd18d1f8370f78f2716727c16a5da0
ryanlovett <rylo@berkeley.edu>
2017-08-04 17:08:01 -0700

    Install ansible on k8s master.

--- bootstrap/setup.bash
+++ bootstrap/setup.bash
@@ -2,6 +2,17 @@
 
 set -e
 
+function proxy_public_ip {
+	kubectl --namespace=jupyterhub get svc proxy-public | \
+		grep proxy-public | awk '{ print $3; }'
+}
+
+# run ansible
+add-apt-repository -y ppa:ansible/ansible
+apt update > /dev/null
+apt -y install ansible
+ansible-playbook -i hosts playbook.yml
+
 curl -s -S https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
 helm init
 
@@ -14,14 +25,12 @@ helm repo update
 
 helm install jupyterhub/jupyterhub --version=v0.4 --name=jupyterhub --namespace=jupyterhub -f config.yaml
 
-kubectl apply -f pv.yaml
+# kubectl apply -f pv.yaml
 
-PUBLIC_IP=$(kubectl --namespace=jupyterhub get svc proxy-public | grep proxy-public | awk '{ print $3; }')
-
+PUBLIC_IP=$(proxy_public_ip)
 while [ "${PUBLIC_IP}" == '<pending>' ]; do
-
-    PUBLIC_IP=$(kubectl --namespace=jupyterhub get svc proxy-public | grep proxy-public | awk '{ print $3; }')
-    sleep 10s;
+    PUBLIC_IP=$(proxy_public_ip)
+    sleep 10s
 done
 
 echo ${PUBLIC_IP}
